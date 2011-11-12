64c1de5113a61f2384163af7c9a9a2772d7a9113
Jesse Lu <mr.jesselu@gmail.com>
2011-11-11 16:02:26 -0800

    Updated documentation, still working on code.

--- lip_bfgs.m
+++ lip_bfgs.m
@@ -1,43 +1,46 @@
-function [] = lbfgs_ip(fun, x, l, u, A, b, ...
+function [] = lip_bfgs(grad, x, l, u, A, b, ...
                         mu_0, sigma, tau, eta, alpha, beta, err_tol, t_min)
 % L-BFGS Interior-Point algorithm.
-% Assumes A is skinny and has linearly independent columns.
+% Assumes A is fat and has linearly independent rows.
 
     %
     % Set up variables and helper functions.
     %
 
 % Choose initial values of variables
+% TODO: change initial s to allow for infeasible start.
 s0 = x - l; % Slack variable for lower bound.
-z0 = ones(length(l), 1); % Dual variable for lower bound.
+z0 = zeros(length(l), 1); % Dual variable for lower bound.
 s1 = u - x; % Slack variable for upper bound.
-z1 = ones(length(u), 1); % Dual variable for upper bound.
+z1 = zeros(length(u), 1); % Dual variable for upper bound.
 y = zeros(length(b), 1); % Dual variable for equality constraint.
 
+n = length(x);
+
 % RHS of KKT equation.
-kkt_res = @(x, s0, s1, y, z0, z1, mu) cat(1, ...
-                        fun.g(x) - A' * y ...
+kkt_res = @(g, x, s0, s1, y, z0, z1, mu) cat(1, ...
+                        g - A' * y ...
                         - z0 + (z0 ./ s0) .* (x - l) - mu * s0.^-1 ...
                         + z1 - (z1 ./ s1) .* (u - x) + mu * s1.^-1, ...
                         A * x - b);
-
-% Merit function.
-phi = @(x, s0, s1, y, z0, z1, mu, alpha_prim, alpha_dual, p, t) ...
-    norm(kkt_res(   x + t * alpha_prim * p.x, ...
-                    s0 + t * alpha_prim * p.s0, ...
-                    s1 + t * alpha_prim * p.s1, ...
-                    y + 1 * alpha_dual * p.y, ...
-                    z0 + 1 * alpha_dual * p.z0, ...
-                    z1 + 1 * alpha_dual * p.z1, mu));
-
 % Error function.
-err = @(x, s0, s1, y, z0, z1, mu) max(cat(1, ...
-                        norm(fun.g(x) - A' * y - z0 + z1), ...
+err = @(g, x, s0, s1, y, z0, z1, mu) 1/sqrt(n) * max(cat(1, ...
+                        norm(g - A' * y - z0 + z1), ...
                         norm(s0 .* z0 - mu), ...
                         norm(s1 .* z1 - mu), ...
                         norm(A * x - b), ...
                         norm(x - l - s0), ...
                         norm(u - x - s1)));
+
+% Merit function.
+phi = @(g, x, s0, s1, y, z0, z1, mu, alpha_prim, alpha_dual, p, t) ...
+    (err(   g, ...
+                    x + t * alpha_prim * p.x, ...
+                    s0 + t * alpha_prim * p.s0, ...
+                    s1 + t * alpha_prim * p.s1, ...
+                    y + 1 * alpha_dual * p.y, ...
+                    z0 + 1 * alpha_dual * p.z0, ...
+                    z1 + 1 * alpha_dual * p.z1, mu));
 
 % Helper to calculate different components of p
 calc_p_xy = @(p) struct(    'x', p(1:length(x)), ...
@@ -55,23 +58,30 @@ f2b_rule = @(pz, z) min([1; my_pos(-tau*z./pz)]);
     %
     % Optimize!
     %
-
-hist.err(1) = err(x, s0, s1, y, z0, z1, 0);
+g = grad(x);
+hist.err(1) = err(g, x, s0, s1, y, z0, z1, 0);
 hist.t(1) = nan;
-n_max = 5;
+hist.grad_evals(1) = 1;
+hist.search_fail(1) = false;
+hist.mu = mu_0;
+n_max = 10;
 h = [];
-tic
 mu = mu_0;
+
+start_time = tic;
+t_disp = 1;
+cnt_display = 0;
 
 while hist.err(end) > err_tol
 
     % L-BFGS approximation of Hessian function.
-    [delta, M, W, h] = lbfgs_update(x, fun.g(x), n_max, h); 
+    [delta, M, W, h] = lbfgs_update(x, grad(x), n_max, h); 
     W = [W; zeros(size(A, 1), size(W, 2))];
 
     % Obtain search direction (p).
+    g = grad(x);
     p = arrow_solve(delta + z0./s0 + z1./s1, A, -W*M, W, ...
-        -kkt_res(x, s0, s1, y, z0, z1, mu));
+        -kkt_res(g, x, s0, s1, y, z0, z1, mu));
 
     % Split up p into various components.
     p = calc_p_xy(p);
@@ -85,37 +95,80 @@ while hist.err(end) > err_tol
     alpha_dual = f2b_rule([p.z0; p.z1], [z0; z1]);
 
     % Perform a backtracking (Armijo) line search.
-    t = backtrack_linesearch(@(t) ...
-        phi(x, s0, s1, y, z0, z1, mu, alpha_prim, alpha_dual, p, t), ...
-        alpha_prim, alpha, beta, t_min);
+    [g, t, hist.grad_evals(end+1), hist.search_fail(end+1)] = my_backtrack_linesearch(...
+        @(t) grad(x + t * alpha_prim * p.x), ...
+        @(g, t) ...
+        phi(g, x, s0, s1, y, z0, z1, mu, alpha_prim, alpha_dual, p, t), ...
+        g, ...
+        1.0, alpha, beta, t_min);
+    if (hist.search_fail(end) == true) % Restart BFGS.
+        h = []
+        t = 0;
+    else
+        % Update variables.
+        x = x + t * alpha_prim * p.x;
+        s0 = s0 + t * alpha_prim * p.s0;
+        s1 = s1 + t * alpha_prim * p.s1;
+        y = y + 1 * alpha_dual * p.y;
+        z0 = z0 + 1 * alpha_dual * p.z0;
+        z1 = z1 + 1 * alpha_dual * p.z1;
+    end
 
-    % Update variables.
-    x = x + t * alpha_prim * p.x;
-    s0 = s0 + t * alpha_prim * p.s0;
-    s1 = s1 + t * alpha_prim * p.s1;
-    y = y + 1 * alpha_dual * p.y;
-    z0 = z0 + 1 * alpha_dual * p.z0;
-    z1 = z1 + 1 * alpha_dual * p.z1;
 
     % Calculate error.
-    hist.err(end+1) = err(x, s0, s1, y, z0, z1, 0);
+    hist.err(end+1) = err(g, x, s0, s1, y, z0, z1, 0);
     hist.t(end+1) = t;
+    hist.mu(end+1) = mu;
 
     % Update mu.
-    if ((hist.err(end) / (length(x)/eta)) <= mu)
-        mu = hist.err(end) / (length(x)/eta);
+    if ((hist.err(end) / eta / sqrt(n)) <= mu)
+        mu = hist.err(end) / eta / sqrt(n);
+    end
+
+    % Output progress.
+    if (toc(start_time) - cnt_display * t_disp > t_disp)
+        if (cnt_display == 0)
+            fprintf('Iter#    Gevals        Error        Mu\n');
+            fprintf('-----    ------        -----        --\n');
+        end
+
+        fprintf('%5d    %6d   %1.4e   %1.1e\n', ...
+            length(hist.err) - 1, sum(hist.grad_evals), hist.err(end), hist.mu(end));
+        cnt_display = cnt_display + 1;
+        semilogy(cumsum(hist.grad_evals), [hist.err; hist.t]', '.-');
+        drawnow
     end
 end
-time0 = toc;
+run_time = toc(start_time);
 
-% Plot results.
-semilogy(0:length(hist.err)-1, [hist.err; hist.t]', '.-');
-xlabel('Error in KKT equations');
-ylabel('iterations');
-title('Interior Primal-Dual Full Newton Step Convergence');
-legend({'global error', 'step size'}, -1);
-drawnow
+        semilogy(cumsum(hist.grad_evals), [hist.err; hist.t]', '.-');
+        drawnow
+% % Plot results.
+% semilogy(0:length(hist.err)-1, [hist.err; hist.t]', '.-');
+% xlabel('Error in KKT equations');
+% ylabel('iterations');
+% title('Interior Primal-Dual Full Newton Step Convergence');
+% legend({'global error', 'step size'}, -1);
+% drawnow
 
-fprintf('Time: %1.2fs, Final error: %e.\n', time0, hist.err(end));
+fprintf('Time: %1.2fs, Final error: %e.\n', run_time, hist.err(end));
 
 
+function [g, t, grad_evals, search_fail] = ...
+    my_backtrack_linesearch(grad, f, g0, t, alpha, beta, t_min);
+% Backtracking line search on one-dimensional function f.
+% t = 1;
+f0 = f(g0, 0);
+g = grad(t);
+grad_evals = 1;
+while f(g, t) > (1 - alpha * t) * f0
+    t = beta * t;
+    g = grad(t);
+    grad_evals = grad_evals + 1;
+    if (t <= t_min) 
+        warning('Backtracking line search failed.');
+        search_fail = true;
+        return
+    end
+end
+search_fail = false;
--- test.m
+++ test.m
@@ -10,11 +10,12 @@ randn('state', 1);
 
 % Function to minimize (quadratic, convex).
 A = spdiags(randn(n, 5), -2:2, n, n);  % Use sparse matrix to speed things up.
+A = A' * A;
 % A = randn(n);
 b = randn(n, 1);
-fun.f = @(x) 0.5 * norm(A * x - b)^2;
-fun.g = @(x) A' * (A * x - b);
-fun.h = @(x) A' * A;
+fun.f = @(x) 0.5 * x' * (A * x - b);
+fun.g = @(x) (A * x - b);
+% fun.h = @(x) A' * A;
 
 fun.f_cvx = @(x) norm(A * x - b);
 
@@ -28,5 +29,5 @@ l = zeros(n, 1);
 u = ones(n, 1);
 
 subplot 111;
-lip_bfgs(fun, 0.5 * ones(n, 1), l, u, A_eq, b_eq, ...
-    1, 0.1, 0.995, 1, 1e-4, 0.5, err_tol, 1e-6);
+lip_bfgs(fun.g, 0.5 * ones(n, 1), l, u, A_eq, b_eq, ...
+    1, 0.1, 0.995, 10, 1e-4, 0.5, err_tol, 1e-3);
